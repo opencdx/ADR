cd35385ecac842429128bbedddf714515041dbf1
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-07-11 15:10:32 -0400

    Allow all CORS cross origin

--- src/main/java/cdx/opencdx/adr/AdrApplication.java
+++ src/main/java/cdx/opencdx/adr/AdrApplication.java
@@ -3,15 +3,22 @@ package cdx.opencdx.adr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @ComponentScan(basePackages = {"cdx.opencdx"})
 @SpringBootApplication
-public class AdrApplication {
+public class AdrApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(AdrApplication.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
