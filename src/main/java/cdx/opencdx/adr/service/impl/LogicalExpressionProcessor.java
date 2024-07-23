0900e35d44f981bb83d0a6bb078134e3305307fc
Jeffery Miller <jeff@mymiller.name>
2024-07-23 12:24:31 -0400

    Update

--- src/main/java/cdx/opencdx/adr/service/impl/LogicalExpressionProcessor.java
+++ src/main/java/cdx/opencdx/adr/service/impl/LogicalExpressionProcessor.java
@@ -32,44 +32,53 @@ public class LogicalExpressionProcessor implements OpenCDXANFProcessor {
      */
     @Override
     public void processAnfStatement(AnfStatementModel anfStatement) {
-        List<LogicalExpressionModel> list = new ArrayList<>();
+        
 
-        list.addAll(anfStatement.getAssociatedStatements().stream().map(AssociatedStatementModel::getSemantic).filter(Objects::nonNull).toList());
-        list.add(anfStatement.getSubjectOfInformation());
-        list.add(anfStatement.getTopic());
-        list.add(anfStatement.getType());
-        list.add(anfStatement.getSubjectOfRecord().getCode());
-        list.addAll(anfStatement.getAuthors().stream().map(PractitionerModel::getCode).filter(Objects::nonNull).toList());
+        this.updateTinkarConceptModel(anfStatement.getAssociatedStatements().stream().map(AssociatedStatementModel::getSemantic).filter(Objects::nonNull).toList(), anfStatement);
+        this.updateTinkarConceptModel(anfStatement.getSubjectOfInformation(), anfStatement);
+        this.updateTinkarConceptModel(anfStatement.getTopic(), anfStatement);
+        this.updateTinkarConceptModel(anfStatement.getType(), anfStatement);
+        this.updateTinkarConceptModel(anfStatement.getSubjectOfRecord().getCode(), anfStatement);
+        this.updateTinkarConceptModel(anfStatement.getAuthors().stream().map(PractitionerModel::getCode).filter(Objects::nonNull).toList(), anfStatement);
         if(anfStatement.getPerformanceCircumstance() != null) {
-            list.add(anfStatement.getPerformanceCircumstance().getStatus());
+            this.updateTinkarConceptModel(anfStatement.getPerformanceCircumstance().getStatus(), anfStatement);
             if(anfStatement.getPerformanceCircumstance().getHealthRisk() !=  null) {
-                list.add(anfStatement.getPerformanceCircumstance().getHealthRisk());
+                this.updateTinkarConceptModel(anfStatement.getPerformanceCircumstance().getHealthRisk(), anfStatement);
             }
-            list.addAll(anfStatement.getPerformanceCircumstance().getPurposes());
+            this.updateTinkarConceptModel(anfStatement.getPerformanceCircumstance().getPurposes(), anfStatement);
         } else if (anfStatement.getRequestCircumstance() != null) {
-            list.addAll(anfStatement.getRequestCircumstance().getConditionalTrigger().stream().map(AssociatedStatementModel::getSemantic).filter(Objects::nonNull).toList());
-            list.add(anfStatement.getRequestCircumstance().getPriority());
-            list.addAll(anfStatement.getRequestCircumstance().getPurposes());
+            this.updateTinkarConceptModel(anfStatement.getRequestCircumstance().getConditionalTrigger().stream().map(AssociatedStatementModel::getSemantic).filter(Objects::nonNull).toList(), anfStatement);
+            this.updateTinkarConceptModel(anfStatement.getRequestCircumstance().getPriority(), anfStatement);
+            this.updateTinkarConceptModel(anfStatement.getRequestCircumstance().getPurposes(), anfStatement);
         } else if(anfStatement.getNarrativeCircumstance() != null) {
-            list.addAll(anfStatement.getNarrativeCircumstance().getPurposes());
+            this.updateTinkarConceptModel(anfStatement.getNarrativeCircumstance().getPurposes(), anfStatement);
+        };
+    }
+    private void updateTinkarConceptModel(List<LogicalExpressionModel> models, AnfStatementModel anfStatement) {
+        if(models == null) {
+            return;
+        }
+        models.forEach(model -> this.updateTinkarConceptModel(model, anfStatement));
+    }
+    private void updateTinkarConceptModel(LogicalExpressionModel model, AnfStatementModel anfStatement) {
+        if(model == null) {
+            return;
+        }
+        
+        TinkarConceptModel work = this.ikmService.getInkarConceptModel(model);
+        
+        if(this.tinkarConceptRepository.existsByConceptId(work.getConceptId())) {
+            work = this.tinkarConceptRepository.findByConceptId(work.getConceptId());
+        } else {
+            work = this.tinkarConceptRepository.save(work);
         }
 
-        list.stream().distinct().map(this.ikmService::getInkarConceptModel).filter(Objects::nonNull).forEach(tinkar -> {
-            TinkarConceptModel work;
-            if(this.tinkarConceptRepository.existsByConceptId(tinkar.getConceptId())) {
-                work = this.tinkarConceptRepository.findByConceptId(tinkar.getConceptId());
-            } else {
-                work = this.tinkarConceptRepository.save(tinkar);
-            }
+        log.info("Tinkar Concept: {}", work);
 
-            log.info("Tinkar Concept: {}", work);
-
-            Optional<AnfStatementModel> first = tinkar.getAnfStatements().stream().filter(Objects::nonNull).filter(anf -> anf.getId().equals(anfStatement.getId())).findFirst();
-            if(first.isEmpty()) {
-                tinkar.getAnfStatements().add(anfStatement);
-                this.tinkarConceptRepository.save(work);
-            }
-
-        });
+        Optional<AnfStatementModel> first = work.getAnfStatements().stream().filter(Objects::nonNull).filter(anf -> anf.getId().equals(anfStatement.getId())).findFirst();
+        if(first.isEmpty()) {
+            work.getAnfStatements().add(anfStatement);
+            this.tinkarConceptRepository.save(work);
+        }
     }
 }
