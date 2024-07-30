dd2b06a0de983f03ca424fec2788bda2b91b9010
Jeffery Miller <jeff@mymiller.name>
2024-07-30 15:10:10 -0400

    Update for UUID

--- src/main/java/cdx/opencdx/adr/service/impl/OpenCDXIKMServiceImpl.java
+++ src/main/java/cdx/opencdx/adr/service/impl/OpenCDXIKMServiceImpl.java
@@ -38,101 +38,101 @@ public class OpenCDXIKMServiceImpl implements OpenCDXIKMService {
         this.conceptRepository = conceptRepository;
 
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("628a2165-0999-40f5-87d4-a40501f1f5f9"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("628a2165-0999-40f5-87d4-a40501f1f5f9"), "Man (person)", "339947000 | Man (person)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("628a2165-0999-40f5-87d4-a40501f1f5f9"), "Man", "339947000 | Man (person)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("c4e07b26-88f9-4250-803c-86463391c001"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("c4e07b26-88f9-4250-803c-86463391c001"), "Woman (person)", "224526002 | Woman (person)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("c4e07b26-88f9-4250-803c-86463391c001"), "Woman", "224526002 | Woman (person)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("359f77d5-6397-4b55-a2b6-c0695f165371"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("359f77d5-6397-4b55-a2b6-c0695f165371"), "Pregnant woman (person)", "255409004 | Pregnant woman (person)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("359f77d5-6397-4b55-a2b6-c0695f165371"), "Pregnant woman", "255409004 | Pregnant woman (person)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("c59c3129-3f64-45e6-9901-6d7f053f6263"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("c59c3129-3f64-45e6-9901-6d7f053f6263"), "Negative (qualifier value)", "260385009 | Negative (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("c59c3129-3f64-45e6-9901-6d7f053f6263"), "Negative", "260385009 | Negative (qualifier value)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("7d540808-89e1-447a-a34b-7876369754b1"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("7d540808-89e1-447a-a34b-7876369754b1"), "Not Detected (qualifier value)", "260415000 | Not Detected (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("7d540808-89e1-447a-a34b-7876369754b1"), "Not Detected", "260415000 | Not Detected (qualifier value)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("5b6a7283-d643-47e6-a2b6-03e49902a45e"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("5b6a7283-d643-47e6-a2b6-03e49902a45e"), "Positive (qualifier value)", "10828004 | Positive (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("5b6a7283-d643-47e6-a2b6-03e49902a45e"), "Positive", "10828004 | Positive (qualifier value)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("4c197306-450d-4a5e-a468-011a36388383"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("4c197306-450d-4a5e-a468-011a36388383"), "Presumptive Positive (qualifier value)", "720735008 | Presumptive Positive (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("4c197306-450d-4a5e-a468-011a36388383"), "Presumptive Positive", "720735008 | Presumptive Positive (qualifier value)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("9a6877c0-04b7-4d5d-9766-8695192790d4"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("9a6877c0-04b7-4d5d-9766-8695192790d4"), "day (qualifier value)", "258703001 | day (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("9a6877c0-04b7-4d5d-9766-8695192790d4"), "day", "258703001 | day (qualifier value)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("3a44167a-e94a-4962-8d2e-d94e57475732"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("3a44167a-e94a-4962-8d2e-d94e57475732"), "month (qualifier value)", "258706009 | month (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("3a44167a-e94a-4962-8d2e-d94e57475732"), "month", "258706009 | month (qualifier value)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("d9036e1e-3397-4f00-a40a-021626644970"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("d9036e1e-3397-4f00-a40a-021626644970"), "year (qualifier value)", "258707000 | year (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("d9036e1e-3397-4f00-a40a-021626644970"), "year", "258707000 | year (qualifier value)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("99782a3a-2e09-4482-9c4c-2213f69792e5"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("99782a3a-2e09-4482-9c4c-2213f69792e5"), "Normal body mass index (finding)", "35425004 | Normal body mass index (finding)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("99782a3a-2e09-4482-9c4c-2213f69792e5"), "Normal body mass index", "35425004 | Normal body mass index (finding)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("b66571ca-bba7-4a5a-a90f-2ef9f0c33a56"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("b66571ca-bba7-4a5a-a90f-2ef9f0c33a56"), "Seconds (qualifier value)", "257997001 | Seconds (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("b66571ca-bba7-4a5a-a90f-2ef9f0c33a56"), "Seconds", "257997001 | Seconds (qualifier value)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("9f19e2d2-3992-4474-b717-e1050e3a5341"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("9f19e2d2-3992-4474-b717-e1050e3a5341"), "Patient (person)", "116154003 | Patient (person)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("9f19e2d2-3992-4474-b717-e1050e3a5341"), "Patient", "116154003 | Patient (person)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("4c3137e7-9215-415e-b840-5a580c275080"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("4c3137e7-9215-415e-b840-5a580c275080"), "Family medicine specialist (occupation)", "62247001 | Family medicine specialist (occupation)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("4c3137e7-9215-415e-b840-5a580c275080"), "Family medicine specialist", "62247001 | Family medicine specialist (occupation)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("12e11755-845a-4e0c-9f39-24f7386a11ae"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("12e11755-845a-4e0c-9f39-24f7386a11ae"), "Evaluation procedure (procedure)", "386053000 | Evaluation procedure (procedure)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("12e11755-845a-4e0c-9f39-24f7386a11ae"), "Evaluation procedure", "386053000 | Evaluation procedure (procedure)"));
         }
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("9d11d012-7e48-4738-960f-420d78262a58"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("9d11d012-7e48-4738-960f-420d78262a58"), "Evaluation finding (finding)", "441742003 | Evaluation finding (finding)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("9d11d012-7e48-4738-960f-420d78262a58"), "Evaluation finding", "441742003 | Evaluation finding (finding)"));
         }
 
         if(!this.conceptRepository.existsByConceptId(UUID.fromString("dc12873c-8989-42f8-ac29-c2eecc3e3b69"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("dc12873c-8989-42f8-ac29-c2eecc3e3b69"), "Acute eruption of skin (disorder)", "723010005 | Acute eruption of skin (disorder)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("dc12873c-8989-42f8-ac29-c2eecc3e3b69"), "Acute eruption of skin", "723010005 | Acute eruption of skin (disorder)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("3a01b6d1-9613-42d6-a6b8-828882e69c2a"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("3a01b6d1-9613-42d6-a6b8-828882e69c2a"), "Age (qualifier value)", "397669002 | Age (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("3a01b6d1-9613-42d6-a6b8-828882e69c2a"), "Age", "397669002 | Age (qualifier value)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("9061a0d4-254a-42b2-b786-82e5e9f79661"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("9061a0d4-254a-42b2-b786-82e5e9f79661"), "Age at diagnosis (observable entity)", "423493009 | Age at diagnosis (observable entity)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("9061a0d4-254a-42b2-b786-82e5e9f79661"), "Age at diagnosis", "423493009 | Age at diagnosis (observable entity)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("f99f17a3-5248-472d-b88f-f4c894630084"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("f99f17a3-5248-472d-b88f-f4c894630084"), "Body height (observable entity)", "1153637007 | Body height (observable entity)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("f99f17a3-5248-472d-b88f-f4c894630084"), "Body height", "1153637007 | Body height (observable entity)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("0f0a0956-9019-4006-b74d-71c04d9a9b59"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("0f0a0956-9019-4006-b74d-71c04d9a9b59"), "Body mass index (observable entity)", "60621009 | Body mass index (observable entity)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("0f0a0956-9019-4006-b74d-71c04d9a9b59"), "Body mass index", "60621009 | Body mass index (observable entity)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("64f4704d-d1f3-46d8-8330-780c1768e541"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("64f4704d-d1f3-46d8-8330-780c1768e541"), "Body mass index 25-29 - overweight (finding)", "162863004 | Body mass index 25-29 - overweight (finding)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("64f4704d-d1f3-46d8-8330-780c1768e541"), "Body mass index 25-29 - overweight", "162863004 | Body mass index 25-29 - overweight (finding)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("49d74011-a43a-4258-b8d7-206679349384"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("49d74011-a43a-4258-b8d7-206679349384"), "Body mass index 30+ - obesity (finding)", "162864005 | Body mass index 30+ - obesity (finding)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("49d74011-a43a-4258-b8d7-206679349384"), "Body mass index 30+ - obesity", "162864005 | Body mass index 30+ - obesity (finding)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("d9188b17-d87e-4f70-92f1-8a9848f2c30f"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("d9188b17-d87e-4f70-92f1-8a9848f2c30f"), "Body mass index less than 20 (finding)", "310252000 | Body mass index less than 20 (finding)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("d9188b17-d87e-4f70-92f1-8a9848f2c30f"), "Body mass index less than 20", "310252000 | Body mass index less than 20 (finding)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("1339a965-5c4d-40a7-b04f-60a17b43455b"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("1339a965-5c4d-40a7-b04f-60a17b43455b"), "Body weight (observable entity)", "27113001 | Body weight (observable entity)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("1339a965-5c4d-40a7-b04f-60a17b43455b"), "Body weight", "27113001 | Body weight (observable entity)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("f99f79f5-1075-4331-b0a8-52d7c647715c"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("f99f79f5-1075-4331-b0a8-52d7c647715c"), "Chronic lung disease (disorder)", "413839001 | Chronic lung disease (disorder)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("f99f79f5-1075-4331-b0a8-52d7c647715c"), "Chronic lung disease", "413839001 | Chronic lung disease (disorder)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("547b6170-4c31-416e-81c7-12625542c72e"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("547b6170-4c31-416e-81c7-12625542c72e"), "Chronic lung disease due to surfactant disorder (disorder)", "707534000 | Chronic lung disease due to surfactant disorder (disorder)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("547b6170-4c31-416e-81c7-12625542c72e"), "Chronic lung disease due to surfactant disorder", "707534000 | Chronic lung disease due to surfactant disorder (disorder)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("03918653-4a61-4282-b146-9b68185e467f"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("03918653-4a61-4282-b146-9b68185e467f"), "Date of birth (observable entity)", "184099003 | Date of birth (observable entity)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("03918653-4a61-4282-b146-9b68185e467f"), "Date of birth", "184099003 | Date of birth (observable entity)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("c6f9e586-4173-43d4-9008-e50c1b7a7a2f"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("c6f9e586-4173-43d4-9008-e50c1b7a7a2f"), "Date of onset (observable entity)", "298059007 | Date of onset (observable entity)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("c6f9e586-4173-43d4-9008-e50c1b7a7a2f"), "Date of onset", "298059007 | Date of onset (observable entity)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("90f740b3-f9a2-4532-8388-b31714a6a6f6"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("90f740b3-f9a2-4532-8388-b31714a6a6f6"), "Detected (qualifier value)", "260373001 | Detected (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("90f740b3-f9a2-4532-8388-b31714a6a6f6"), "Detected", "260373001 | Detected (qualifier value)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("b78510a4-f56a-4e61-b7a4-c8438c673d1b"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("b78510a4-f56a-4e61-b7a4-c8438c673d1b"), "History of chronic lung disease (situation)", "414415007 | History of chronic lung disease (situation)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("b78510a4-f56a-4e61-b7a4-c8438c673d1b"), "History of chronic lung disease", "414415007 | History of chronic lung disease (situation)"));
         }
         if (!this.conceptRepository.existsByConceptId(UUID.fromString("b3a45188-79f6-457c-b2b6-957780e84736"))) {
-            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("b3a45188-79f6-457c-b2b6-957780e84736"), "Invalid result (qualifier value)", "455371000124106 | Invalid result (qualifier value)"));
+            this.conceptRepository.save(new TinkarConceptModel(UUID.fromString("b3a45188-79f6-457c-b2b6-957780e84736"), "Invalid result", "455371000124106 | Invalid result (qualifier value)"));
         }
 
         if(!this.conceptRepository.existsByConceptId(UUID.fromString(OpenCDXIKMServiceImpl.UNIT_METER))) {
@@ -160,12 +160,20 @@ public class OpenCDXIKMServiceImpl implements OpenCDXIKMService {
      */
     @Override
     public TinkarConceptModel getInkarConceptModel(LogicalExpression logicalExpression) {
-        log.info("Getting concept model for expression: {}", logicalExpression.getExpression());
-        TinkarConceptModel model = conceptRepository.findByConceptDescription(logicalExpression.getExpression());
-        if (model == null) {
-            model = conceptRepository.findByConceptId(UUID.fromString(logicalExpression.getExpression()));
-        }
+        UUID conceptId = this.testAndConvert(logicalExpression.getExpression());
 
-        return model;
+        if(conceptId != null) {
+            return conceptRepository.findByConceptId(conceptId);
+        } else {
+            return conceptRepository.findByConceptDescription(logicalExpression.getExpression());
+        }
+    }
+
+    private UUID testAndConvert(String uuidString) {
+        try {
+            return UUID.fromString(uuidString);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
     }
 }
