235c13a1248c2f754d64f232051bcc4f3d86dfbe
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-10-09 14:14:01 -0400

    Only get distinct results

--- src/main/java/cdx/opencdx/adr/controller/QueryController.java
+++ src/main/java/cdx/opencdx/adr/controller/QueryController.java
@@ -129,7 +129,7 @@ public class QueryController {
 
     @GetMapping("/search")
     public ResponseEntity<List<TinkarConceptModel>> search(@RequestParam String search) {
-        return ResponseEntity.ok(ikmInterface.search(search, 30).stream().map(publicId -> {
+        return ResponseEntity.ok(ikmInterface.search(search, 30).stream().distinct().map(publicId -> {
             TinkarConceptModel model = new TinkarConceptModel();
             model.setConceptName(PrimitiveData.textOptional(EntityService.get().nidForPublicId(publicId)).orElse(null));
             model.setConceptDescription(ikmInterface.descriptionsOf(List.of(publicId)).getFirst());
